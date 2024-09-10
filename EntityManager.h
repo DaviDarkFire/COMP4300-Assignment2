e56d237e710835f8029c60fe3e03f18ee80b664b
Davi Ferreira Santo <daviferreirasantog@gmail.com>
2024-09-09 22:29:13 -0400

    adding more to the skelleton of the project

---
+++ EntityManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Entity.h"
+#include <string>
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec;
+typedef std::map<std::string, EntityVec>     EntityMap;
+
+class EntityManager
+{
+
+private:
+	EntityVec m_entities;
+	EntityMap m_entityMap;
+	EntityVec m_toAdd;
+	size_t    m_totalEntities{ 0 };
+
+	void removeDeadEntities(EntityVec & vec);
+
+public:
+
+	EntityManager();
+
+	void update();
+
+	std::shared_ptr<Entity> addEntity(const std::string & tag);
+
+	const EntityVec & getEntities();
+	const EntityVec & getEntities(const std::string & tag);
+};
