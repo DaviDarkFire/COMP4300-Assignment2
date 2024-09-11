e893f672283ddbb170a9fc88d779e8aa2c01141f
Davi Ferreira Santo <daviferreirasantog@gmail.com>
2024-09-10 21:37:38 -0400

    adding EntityManager.cpp and testing the Entity and EntityManager creation on nmain.cpp

---
+++ EntityManager.cpp
@@ -0,0 +1,12 @@
+#include "EntityManager.h"
+#include "Entity.h"
+#include <memory>
+
+
+EntityManager::EntityManager(){}
+
+std::shared_ptr<Entity> EntityManager::addEntity(const std::string & tag)
+{
+    
+    return nullptr;
+}
